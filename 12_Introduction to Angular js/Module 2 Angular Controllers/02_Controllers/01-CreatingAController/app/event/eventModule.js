f90263a3cbf39756f2e06252832754bb2fc580ab
abdibogor <bogoreh_code@outlook.com>
2019-07-17 17:37:07 +0300

    The ninth commit

---
+++ 12_Introduction to Angular js/Module 2 Angular Controllers/02_Controllers/01-CreatingAController/app/event/eventModule.js
@@ -0,0 +1,45 @@
+
+
+(function(){
+
+angular.module('eventModule', [])
+.config([function () {
+
+	
+console.log("Event Module:: config");
+
+}])
+
+.run([function () {
+
+    console.log("Event Module::running");
+
+}])
+
+.controller('EventCtrl', ['$scope', function ($scope) {
+	
+		$scope.title = "Young Game Maker";
+	
+		$scope.menu=[
+		
+			{
+
+			name:"Events",
+			
+			href:"index.html"
+		
+			},
+	
+		{
+ 
+		name:"Contact",
+			
+		href:"contact.html"
+		
+					}
+	
+						
+		]
+}])
+
+})();
